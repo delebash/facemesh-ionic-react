92f73f27bafb5290c6b14a314041e27a398e0402
delebash <danelebash@hotmail.com>
2020-07-06 03:12:53 -0400

    get selected camera

--- src/pages/FaceRecorder.tsx
+++ src/pages/FaceRecorder.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import {
-    IonLabel, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonTabButton, IonTabBar,IonIcon,
+    IonLabel, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonTabButton, IonTabBar, IonIcon,
     useIonViewDidLeave, useIonViewDidEnter
 } from '@ionic/react';
 import './FaceRecorder.css';
@@ -8,33 +8,36 @@ import './FaceRecorder.css';
 import {recording, stop} from 'ionicons/icons';
 
 const FaceRecorder: React.FC = () => {
-if  (document.getElementById("camera")){
-    let selectedCamera = (document.getElementById("camera") as HTMLInputElement).value
-     console.log(selectedCamera);
-}
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Face Recorder</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent>
-              <IonTabBar>
-                  <IonTabButton  tab="FaceRecorder" href="/FaceRecorder">
-                      <IonIcon icon={recording} class="recording" />
-                      <IonLabel>Record</IonLabel>
-                  </IonTabButton>
-                  <IonTabButton tab="FaceRecorder" href="/FaceRecorder">
-                      <IonIcon icon={stop} class="stop"/>
-                      <IonLabel>Stop</IonLabel>
-                  </IonTabButton>
-              </IonTabBar>
-          <video id="gum"></video>
-      </IonContent>
-    </IonPage>
-  );
+    useIonViewDidEnter(() => {
+        if (document.getElementById("camera")) {
+            let selectedCamera = (document.getElementById("camera") as HTMLInputElement).value
+            console.log(selectedCamera);
+        }
+    })
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Face Recorder</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent>
+                <IonTabBar>
+                    <IonTabButton tab="FaceRecorder" href="/FaceRecorder">
+                        <IonIcon icon={recording} class="recording"/>
+                        <IonLabel>Record</IonLabel>
+                    </IonTabButton>
+                    <IonTabButton tab="FaceRecorder" href="/FaceRecorder">
+                        <IonIcon icon={stop} class="stop"/>
+                        <IonLabel>Stop</IonLabel>
+                    </IonTabButton>
+                </IonTabBar>
+                <video id="gum"></video>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default FaceRecorder;
